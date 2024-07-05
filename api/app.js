a3683291bfa86e1ea1c366501c4f843dd0f61669
Kelvin Ebima <kelvinebima@gmail.com>
2024-07-05 14:31:14 -0700

    implement sign up form submission and user creation in db

--- api/app.js
+++ api/app.js
@@ -1,12 +1,14 @@
 import express from "express";
+import cors from "cors";
 import cookieParser from "cookie-parser";
 import postRoute from "./routes/post.route.js";
 import authRoute from "./routes/auth.route.js";
 
 const app = express();
 
-app.use(express.json())
-app.use(cookieParser())
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(express.json());
+app.use(cookieParser());
 
 app.use("/api/posts", postRoute);
 app.use("/api/auth", authRoute);
--- client/src/routes/register/register.jsx
+++ client/src/routes/register/register.jsx
@@ -1,16 +1,37 @@
 import "./register.scss";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
+import { useState } from "react";
 
 function Register() {
-  const handleSubmit = (e) => {
-    e.preventDefault()
+
+  const [error,setError] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setError("")
+    setIsLoading(true);
     const formData = new FormData(e.target);
 
-    const username = formData.get("username")
-    const email = formData.get("email")
-    const password = formData.get("password")
+    const username = formData.get("username");
+    const email = formData.get("email");
+    const password = formData.get("password");
 
-    console.log(username, email, password);
+    try {
+      const res = await axios.post("http://localhost:8800/api/auth/register", {
+        username, email, password,
+      });
+      navigate("/login")
+    } catch (err) {
+      console.log(err)
+      setError(err.response.data.message)
+    } finally {
+      setIsLoading(false);
+    }
+    
 
   };
 
@@ -23,7 +44,8 @@ function Register() {
           <input name="username" type="text" placeholder="Username" />
           <input name="email" type="text" placeholder="Email" />
           <input name="password" type="password" placeholder="Password" />
-          <button >Register</button>
+          <button disabled={isLoading}>Register</button>
+          {error && <span>{error}</span>}
           <Link to="/login">Do you have an account?</Link>
         </form>
       </div>
